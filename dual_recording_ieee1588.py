cb68f01fe2d727ad374071cf33951c96b5dbd5e4
Kunal Shah <kshah@sralab.org>
2022-06-22 10:54:33 -0500

    Adding logic to exit program with any keypress.

--- dual_recording_ieee1588.py
+++ dual_recording_ieee1588.py
@@ -12,6 +12,7 @@ import json
 import time
 import cv2
 import os
+import _thread
 
 # Defining window size based on number
 # of cameras(key)
@@ -168,6 +169,13 @@ def record_dual(vid_file, max_frames=100, num_cams=1, frame_pause=0, preview = T
         for frame in iter(image_queue.get, None):
             cv2.imshow("Preview", cv2.cvtColor(frame['im'], cv2.COLOR_BAYER_RG2RGB))
             cv2.waitKey(1)
+
+            if cv2.waitKey(1) != -1: #ord('q') or cv2.waitKey(1) == ord('c'):
+                print("got here")
+                stop_flag = 1
+                _thread.interrupt_main()
+                # sys.exit()
+                # cv2.destroyAllWindows()
         # try:
         #     f = image_queue.get(block=False)
         # except Empty:
